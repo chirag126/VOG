33915fbeaa1f737cbf1b282978dfabe88f14be7f
Chirag <chiragagarwall12@gmail.com>
2020-10-07 15:16:26 -0500

    finalized toy_script

--- toy_script.py
+++ toy_script.py
@@ -1,10 +1,22 @@
 import ipdb
 import torch
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 12, 'font.weight': 'bold'})
+plt.rc('font', family='sans-serif')
+plt.rcParams["axes.grid"] = False
 
 # CREATE RANDOM DATA POINTS
 from sklearn.datasets import make_blobs
+
+parser = argparse.ArgumentParser(description='VOG for toy dataset')
+parser.add_argument('--mode', type=str, default='early', help='vog analysis for early | middle | late stage')
+parser.add_argument('--vog_cal', type=str, default='normalize', help='raw | normalize | abs_normalize')
+parser.add_argument('--split', type=str, default='train', help='dataset type whose gradients we want to analyze')
+args = parser.parse_args()
+
 
 class Feedforward(torch.nn.Module):
         def __init__(self, input_size, hidden_size):
@@ -12,39 +24,23 @@ class Feedforward(torch.nn.Module):
             self.input_size = input_size
             self.hidden_size  = hidden_size
             self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
-            # self.relu = torch.nn.ReLU()
             self.fc2 = torch.nn.Linear(self.hidden_size, 2)
-            # self.fc3 = torch.nn.Linear(self.hidden_size, 2)
-            # self.sigmoid = torch.nn.ReLU()
         def forward(self, x):
             hidden = self.fc1(x)
-            # relu = self.relu(hidden)
             output = self.fc2(hidden)
-            # output = self.fc3(output)
             return output
-
-
-#class calculate_vog_score():
-#    def __init__(self, grad_list):
-#        self.grad_list = grad_list
-#    
-#    def mean_grad(self, gt_label, pr_label):
-#        u_grad={}
-#        count_class={}
-#        for ind in range(self.grad_list.shape[0]):
-            
+          
         
 def weights_init(m):
     if isinstance(m, torch.nn.Conv3d):
         torch.nn.init.xavier_uniform_(m.weight.data, init.calculate_gain('relu'))
-        # torch.nn.init.xavier_uniform_(m.bias.data)
+        torch.nn.init.xavier_uniform_(m.bias.data)
     elif isinstance(m, torch.nn.BatchNorm3d):
         m.weight.data.normal_(mean=1.0, std=0.02)
         m.bias.data.fill_(0)
     elif isinstance(m, torch.nn.Linear):
         torch.manual_seed(0)
         m.weight.data.normal_(0.0, 1)
-        # torch.nn.init.xavier_uniform_(m.weight.data)
         m.bias.data.fill_(0)
 
 
@@ -72,16 +68,6 @@ def plot_decision_boundaries(X, y, model):
     # Step size of the mesh. Decrease to increase the quality of the VQ.
     h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].    
 
-#    # Plot the decision boundary. For that, we will assign a color to each
-#    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-#    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-
-#    # Meshgrid creation
-#    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-
-#    # Obtain labels for each point in mesh using the model.
-#    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])    
-
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
@@ -96,14 +82,13 @@ def plot_decision_boundaries(X, y, model):
     # Plotting
     plt.figure()
     plt.contourf(xx, yy, Z, alpha=0.4)
-    plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.8)
-    plt.plot(center_0[0], center_0[1], '*r')
-    plt.plot(center_1[0], center_1[1], '*b')
+    plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.8, edgecolors='k', linewidths=4)
     plt.xlabel("Feature-1",fontsize=15)
     plt.ylabel("Feature-2",fontsize=15)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
-    # plt.show()
+    plt.grid('off')
+    # plt.savefig('figure/toy_dataset_decision_boundary.jpg', bbox_inches='tight')
     return plt
 
 
@@ -112,9 +97,14 @@ def blob_label(y, label, loc): # assign labels
     for l in loc:
         target[y == l] = label
     return target
+
 seed=212
 np.random.seed(seed=seed)
-x_train, y_train = make_blobs(n_samples=100, n_features=2, cluster_std=2, shuffle=True)
+X, Y = make_blobs(n_samples=500, n_features=2, cluster_std=2, shuffle=True)
+x_train, x_test, y_train, y_test = X[:450, :], X[-50:, :], Y[:450], Y[-50:]
+
+
+# Visualizing the training dataset
 #plt.figure()
 #plt.plot(x_train[y_train==0, 0], x_train[y_train==0, 1], 'or')
 #plt.plot(x_train[y_train==1, 0], x_train[y_train==1, 1], 'xb')
@@ -124,27 +114,38 @@ x_train, y_train = make_blobs(n_samples=100, n_features=2, cluster_std=2, shuffl
 x_train = torch.FloatTensor(x_train)
 y_train = torch.FloatTensor(blob_label(y_train, 0, [0]))
 y_train = torch.FloatTensor(blob_label(y_train, 1, [1,2,3]))
-np.random.seed(seed=seed)
-x_test, y_test = make_blobs(n_samples=20, n_features=2, cluster_std=2, shuffle=True)
+x_test = torch.FloatTensor(x_test)
+y_test = torch.FloatTensor(blob_label(y_test, 0, [0]))
+y_test = torch.FloatTensor(blob_label(y_test, 1, [1,2,3]))
+
+# Visualizing the testing dataset
 # plt.figure()
 # plt.plot(x_test[y_test==0, 0], x_test[y_test==0, 1], 'or')
 # plt.plot(x_test[y_test==1, 0], x_test[y_test==1, 1], 'xb')
 # plt.show()
 # exit(0)
 
-x_test = torch.FloatTensor(x_test)
-y_test = torch.FloatTensor(blob_label(y_test, 0, [0]))
-y_test = torch.FloatTensor(blob_label(y_test, 1, [1,2,3]))
+# Select the split whose gradient we want to analyze
+if args.split == 'train':
+    grad_X = x_train
+    grad_Y = y_train
+elif args.split == 'test':
+    grad_X = x_test
+    grad_Y = y_test
+else:
+    print('Invalid split type! Exiting..')
+    exit(0)
 
 model = Feedforward(2, 10)
 model.apply(weights_init)
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.001)
 
-epoch = 15
+epochs = 15
 acc_list = []
 vog = {}
-for epoch in range(epoch):
+testing_pred_label = []
+for ep in range(epochs):
     model.train()
     optimizer.zero_grad()
 
@@ -154,11 +155,10 @@ for epoch in range(epoch):
     # Compute Loss
     loss = criterion(y_pred, y_train.type(torch.LongTensor))
     
-    if epoch !=0 and epoch % 1 == 0:  #  and (epoch > 1500 or epoch <= 70):
-        output = torch.argmax(torch.nn.Softmax(dim=1)(y_pred), dim=1)
-        acc = torch.sum(output == y_train).item()/ y_train.shape[0]
-        print('Epoch {}: train loss: {} || train acc: {}'.format(epoch, loss.item(), acc*100))
-        acc_list.append(acc*100)
+    output = torch.argmax(torch.nn.Softmax(dim=1)(y_pred), dim=1)
+    acc = torch.sum(output == y_train).item()/ y_train.shape[0]
+    print('Epoch {}: train loss: {} || train acc: {}'.format(ep, loss.item(), acc*100))
+    acc_list.append(acc*100)
 
     # Backward pass
     loss.backward()
@@ -167,71 +167,99 @@ for epoch in range(epoch):
     # Get gradients
     # Get the index corresponding to the maximum score and the maximum score itself.
     model.eval()
-    x_train.requires_grad = True
-    sel_nodes_shape = y_train.shape
+    grad_X.requires_grad = True
+    sel_nodes_shape = grad_Y.shape
     ones = torch.ones(sel_nodes_shape)
-    logits = model(x_train)
+    logits = model(grad_X)
     probs = torch.nn.Softmax(dim=1)(logits)
-    sel_nodes = probs[torch.arange(len(y_train)), y_train.type(torch.LongTensor)]
+    sel_nodes = probs[torch.arange(len(grad_Y)), grad_Y.type(torch.LongTensor)]
     sel_nodes.backward(ones)
-    grad = x_train.grad.data.numpy()
-    if epoch !=0 and epoch % 1 == 0:  #  and (epoch > 1500 or epoch <= 70):
-        for i in range(x_train.shape[0]):
-            if i not in vog.keys():
-                vog[i] = []
-                vog[i].append(grad[i, :].tolist())
-            else:
-                vog[i].append(grad[i, :].tolist())
+    grad = grad_X.grad.data.numpy()
+    for i in range(grad_X.shape[0]):
+        if i not in vog.keys():
+            vog[i] = []
+            vog[i].append(grad[i, :].tolist())
+        else:
+            vog[i].append(grad[i, :].tolist())
+
+    # Store testing datasets predicted labels
+    y_pred = model(x_test)
+    testing_pred_label.append(torch.argmax(torch.nn.Softmax(dim=-1)(y_pred), dim=1).numpy())
+
+print('### Finished training!! ###')
 
 # TESTING
 model.eval()
-plot_decision_boundaries(x_train.detach(), y_train, model)
 y_pred = model(x_test)
 after_train = criterion(y_pred, y_test.type(torch.LongTensor))
 acc = torch.sum(torch.argmax(torch.nn.Softmax(dim=1)(y_pred), dim=1) == y_test).item()/ y_test.shape[0]
 print('Test loss: {} | test acc: {}'.format(after_train.item(), acc*100))
 
+# Plot decision boundaries of the MLP classifier
+plot_decision_boundaries(grad_X.detach(), grad_Y, model)
 
 # Analysis of Gradients
 training_vog_stats=[]
 training_labels=[]
 training_class_variances = list(list() for i in range(2))
 training_class_variances_stats = list(list() for i in range(2))
-for ii in range(x_train.shape[0]):
-    temp_grad = np.array(vog[ii][7:])
+for ii in range(grad_X.shape[0]):
+    if args.mode == 'early':
+        temp_grad = np.array(vog[ii][:5])
+        temp_pred = np.array(testing_pred_label[:5])[-1]
+    elif args.mode == 'middle':
+        temp_grad = np.array(vog[ii][5:10])
+        temp_pred = np.array(testing_pred_label[5:10])[-1]
+    elif args.mode == 'late':
+        temp_grad = np.array(vog[ii][10:])
+        temp_pred = np.array(testing_pred_label[10:])[-1]
+    elif args.mode == 'complete':
+        temp_grad = np.array(vog[ii])
+        temp_pred = np.array(testing_pred_label)[-1]        
+    else:
+        print('Invalid mode!! Exiting..')
+        exit(0)
     mean_grad = np.sum(np.array(vog[ii]), axis=0)/len(temp_grad)
     training_vog_stats.append(np.mean(np.sqrt(sum([(mm-mean_grad)**2 for mm in temp_grad])/len(temp_grad))))
-    training_labels.append(int(y_train[ii].item()))
-    training_class_variances[int(y_train[ii].item())].append(np.mean(np.sqrt(sum([(mm-mean_grad)**2 for mm in temp_grad])/len(temp_grad))))
+    training_labels.append(int(grad_Y[ii].item()))
+    training_class_variances[int(grad_Y[ii].item())].append(np.mean(np.sqrt(sum([(mm-mean_grad)**2 for mm in temp_grad])/len(temp_grad))))
 
-# Normalized VOG scores
-normalized_vog = []
-for ii in range(x_train.shape[0]):
-    mu = np.mean(training_class_variances[int(y_train[ii].item())])
-    std  = np.std(training_class_variances[int(y_train[ii].item())])
-    normalized_vog.append((training_vog_stats[ii] - mu)/std)
+if args.vog_cal in ['normalize', 'abs_normalize']:
+    # Normalized VOG scores
+    normalized_vog = []
+    for ii in range(grad_X.shape[0]):
+        mu = np.mean(training_class_variances[int(grad_Y[ii].item())])
+        std  = np.std(training_class_variances[int(grad_Y[ii].item())])
+        if args.vog_cal == 'normalize':
+            normalized_vog.append((training_vog_stats[ii] - mu)/std)
+        else:
+            normalized_vog.append(np.abs((training_vog_stats[ii] - mu)/std))
+else:
+    normalized_vog = training_vog_stats
 
-# ipdb.set_trace()
-plt.plot(x_train.detach().numpy()[y_train==0, 0], x_train.detach().numpy()[y_train==0, 1], 'or')
-plt.plot(x_train.detach().numpy()[y_train==1, 0], x_train.detach().numpy()[y_train==1, 1], 'ob')
-# ipdb.set_trace()
-for i in range(x_train.shape[0]):
-    plt.text(x_train.detach().numpy()[i, 0], x_train.detach().numpy()[i, 1], "{:.2f}".format(normalized_vog[i]), ha='left', va='top', family="monospace")
-# plt.show()
+
+plt.plot(grad_X.detach().numpy()[grad_Y==0, 0], grad_X.detach().numpy()[grad_Y==0, 1], 'ob')
+plt.plot(grad_X.detach().numpy()[grad_Y==1, 0], grad_X.detach().numpy()[grad_Y==1, 1], 'or')
+plt.rcParams.update({'font.size': 18, 'font.weight': 'bold'})
+#for i in range(grad_X.shape[0]):
+#    plt.text(grad_X.detach().numpy()[i, 0], grad_X.detach().numpy()[i, 1], "{:.2f}".format(normalized_vog[i]), ha='left', va='top', family="monospace")
+plt.legend(('Class-0', 'Class-1'))
+plt.savefig('figures/toy_dataset_decision_boundary.jpg', bbox_inches='tight')
 
 
 def calculate_distance(x):
-    return np.abs(0.51*x[0] + x[1] - 0.69)/np.sqrt(1.26)
+    return np.abs(0.2471*x[0] + x[1] - 0.0124)/1.0301
 
 # Distance vs. VOG score
 distance_from_boundary = []
-for ii in range(x_train.shape[0]):
-    # ipdb.set_trace()
-    distance_from_boundary.append(calculate_distance(x_train[ii].detach().numpy()))
+for ii in range(grad_X.shape[0]):
+    distance_from_boundary.append(calculate_distance(grad_X[ii].detach().numpy()))
 
 plt.figure()
-plt.scatter(distance_from_boundary, normalized_vog)
-plt.xlabel('Perpendicular distance from the boundary')
-plt.ylabel('Normalized VOG score')
-plt.show()
-    
+plt.scatter(np.array(normalized_vog)[grad_Y==0], np.array(distance_from_boundary)[grad_Y==0], linewidths=1.5, edgecolors='k', c='b', alpha=0.6)
+plt.scatter(np.array(normalized_vog)[grad_Y==1], np.array(distance_from_boundary)[grad_Y==1], linewidths=1.5, edgecolors='k', c='r', alpha=0.6)
+plt.ylabel('Perpendicular distance')
+plt.xlabel('VOG score')
+plt.grid('off')
+plt.legend(('Class-0', 'Class-1'))
+plt.savefig(f'figures/{args.split}_{args.mode}_{args.vog_cal}.jpg', bbox_inches='tight')
